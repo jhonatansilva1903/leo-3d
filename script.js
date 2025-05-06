4c2806681ba42be0ed5537d4992f8e3d92735283
jhonatan <jhonatands1993@gmail.com>
2025-05-06 00:39:27 -0300

    Adiciona página figures e atualiza layout e imagens

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-let currentSlide = 0; // Inicializa com a primeira imagem visível
+let currentSlide = 0;
 
-// Função para mover as imagens do carrossel
 function moveSlide(step) {
   const slides = document.querySelectorAll('.slide');
   const totalSlides = slides.length;
-  
-  // Calcula o novo índice da imagem visível
   currentSlide = (currentSlide + step + totalSlides) % totalSlides;
-
-  // Atualiza a visibilidade das imagens
   slides.forEach((slide, index) => {
     slide.style.display = index === currentSlide ? 'block' : 'none';
   });
 }
 
-// Inicializa o carrossel mostrando a primeira imagem
 document.addEventListener("DOMContentLoaded", () => {
-  moveSlide(0); // Exibe a primeira imagem
+  moveSlide(0);
 });
